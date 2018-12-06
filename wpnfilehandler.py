689deb28a6dd80bbd790aeee2540cf93e3a19199
wad67 <Wad67@live.com.au>
2018-12-06 23:59:53 +1000

    Almost got the thing within thing part working
    Just need to figure out why it isn't checking to see if the thing within the thing is there so it can add it to the thing in order to add it to the thing

--- wpnfilehandler.py
+++ wpnfilehandler.py
@@ -1,5 +1,5 @@
 wpnFile = {}
-
+tempDict = {}
 
 def loadWpnFile(filename):
     print('Attempting to load:', filename)
@@ -45,5 +45,62 @@ def loadWpnFile(filename):
             print(values[0])
             wpnFile[values[0]] = values[1]
         print(line)
+        #
+        #
+        # THINGS WITHIN THINGS SECTION
+        # (Yes I know this is largely redundant but I want to get this fucking thing working first thank ya very much)
+        #
+        if '{' in line and not line[1:].isspace():
+            print('oh fuck its a start of a thing within a thing within a thing')
+            # grab the first thingomabobby in the line
+            line = line.strip()
+            line = line.replace('"', '')
+            line = line.replace(',', '')
+            values = line.split(':')
+            tempKey = values[0]
+            print('thingKey', tempKey)
+            if '"' in line and '[' in line:
+                print('thing within thing: start of multi line entry')
+                line = line.strip()
+                line = line.replace('"', '')
+                line = line.replace(',', '')
+                values = line.split(':')
+                tempKey = values[0]
+                tempList = []
+                print(values)
+                if values[1].isspace():
+                    print('thing within thing: There was something immediately after the opening bracket')
+                    # don't want to deal with this right now so lets hope it never happens
+                while ']' not in line:
+                    # take a look at the next line
+                    print('thing within thing: Taking a look at the next line:')
+                    line = next(file)
+                    print(line)
+                    # if there is something there, strip all whitespace and other junk, then append to list
+                    if len(line) > 0 and ']' not in line:
+                        line = line.replace(' ', '')
+                        line = line.replace(',', '')
+                        line = line.strip()
+                        tempList.append(line)
+                print('thing within thing: Finished looking at multiple lines:')
+                print(tempList)
+                tempDict[tempKey] = tempList
+
+            elif '"' in line:
+                print('thing within thing: Start of an entry:')
+                line = line.strip()
+                line = line.replace('"', '')
+                line = line.replace(',', '')
+                values = line.split(':')
+                print(values[0])
+                tempDict[values[0]] = values[1]
+            print('THING WITHIN A THING DATA:')
+            print('THING NAME:')
+            print(tempKey)
+            print('THING DATA:')
+            print(tempDict)
+
+            wpnFile[tempKey] = tempDict
+    print(wpnFile)
 
     return wpnFile
